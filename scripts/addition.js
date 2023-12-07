ddfc7fb9977e23f7948b1471aa7d5883f4e3d92c
luoqiming82 <qimingluo28@gmail.com>
2023-12-07 21:44:22 +0800

    exp subOp btn event successfully

--- scripts/addition.js
+++ scripts/addition.js
@@ -1,12 +1,20 @@
 // initiate game mod
 const gameMod = {
-    mod: ''
+    mod: '',
+    firstNum: 1
 }
 // elements
 const topChoiceForm = document.getElementById('top-choice-form')
+const subOpForm = document.getElementById('sub-options')
 
 // exp 
 topChoiceForm.addEventListener('click', function(e){
     gameMod.mod = e.target.value
-    document.getElementById('exp').textContent = gameMod.mod
+    document.getElementById('exp').textContent = JSON.stringify(gameMod)
+})
+
+// exp sub-op event
+subOpForm.addEventListener('click', function(e){
+    gameMod.firstNum = Number(e.target.value)
+    document.getElementById('exp').textContent = JSON.stringify(gameMod)
 })
