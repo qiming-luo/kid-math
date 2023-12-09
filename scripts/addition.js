8fae9322461b5e2b41284df34b4fd05c64e69145
luoqiming82 <lowuji22@gmail.com>
2023-12-09 10:47:03 +0800

    finish addition.js random quiz events

--- scripts/addition.js
+++ scripts/addition.js
@@ -19,6 +19,7 @@ const nextBtnRandomQ = document.getElementById('next-btn-randomQ')
 const questionRandom = document.getElementById('question-random')
 const userAnswerRandomQ = document.getElementById('user-answer-randomQ')
 
+// next-btn event (display question)
 nextBtnRandomQ.addEventListener('click', function(){
     // gen 2 random num(1-9)
     let val1 = Math.ceil(Math.random()*9)
@@ -34,4 +35,43 @@ nextBtnRandomQ.addEventListener('click', function(){
     game.randomQuiz.answer = val1 + val2
     // display question
     questionRandom.textContent = `${val1} + ${val2} = `
+    // clear stars and other txt
+    document.getElementById('x-stars-random').innerHTML = ''
+    document.getElementById('y-stars-random').innerHTML = ''
+    document.getElementById('answer-verifier-symbol-random').innerHTML = ''
+    document.getElementById('answer-verifier-text-ranom').textContent = ''
+    document.getElementById('user-answer-randomQ').value = ''
 })
+
+// count stars event
+document.getElementById('count-stars-btn-random').addEventListener('click', function(){
+    let xStars = ''
+    let yStars = ''
+    // asign x and y stars
+    for(let i=1; i<=game.randomQuiz.x; i++){
+        xStars += '&#9734;  '
+    }
+    for(let i=1; i<=game.randomQuiz.y; i++){
+        yStars += '&#9734;  '
+    }
+    // display them
+    document.getElementById('x-stars-random').innerHTML = xStars
+    document.getElementById('y-stars-random').innerHTML = yStars
+})
+
+// check answer event
+document.getElementById('check-answer-btn-random').addEventListener('click', function(){
+    // get user answer
+    const userAnswer = Number(document.getElementById('user-answer-randomQ').value)
+    // verify answer
+    if(userAnswer == game.randomQuiz.answer){
+        document.getElementById('answer-verifier-symbol-random').innerHTML = '&#127799;'
+        document.getElementById('answer-verifier-text-ranom').textContent = 'well done'
+    }else{
+        document.getElementById('answer-verifier-symbol-random').innerHTML = '&#9747;'
+        document.getElementById('answer-verifier-text-ranom').textContent = 'try again'
+    }
+})
+
+// tutorial events ----------------------------------------------------------------------------------------
+
